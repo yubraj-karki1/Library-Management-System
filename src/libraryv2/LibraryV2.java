228232bb5f8e0b7c543bec51f0cfceb517fa775d
yubraj <yubraj.yubrajkarki188@gmail.com>
2024-08-04 09:16:14 +0545

    first commit

---
+++ src/libraryv2/LibraryV2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package libraryv2;
+
+/**
+ *
+ * @author yubra
+ */
+public class LibraryV2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        login loginFrame= new login();
+        loginFrame.setVisible(true);
+        loginFrame.pack();
+        loginFrame.setLocationRelativeTo(null);
+        
+    }
+    
+}
